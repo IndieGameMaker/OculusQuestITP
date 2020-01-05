95051f35dc9615b54014c2796bc3f93b4f47d2c3
Jaehyun Lee <myleje@gmail.com>
2020-01-05 16:16:37 +0900

    .

--- Assets/02.Scripts/GrabMgr.cs
+++ Assets/02.Scripts/GrabMgr.cs
@@ -18,18 +18,21 @@ public class GrabMgr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (grabObject == null) return;
+
         if (isTouched && OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
         {
             grabObject.SetParent(this.transform); //컨트롤러 하위로 차일드화 시킴
             grabObject.GetComponent<Rigidbody>().isKinematic = true;
         }
 
-        if (grabObject != null && OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger))
+        if (OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger))
         {
             grabObject.SetParent(null);
             Vector3 velocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch);
-            grabObject.GetComponent<Rigidbody>().velocity = velocity;
             grabObject.GetComponent<Rigidbody>().isKinematic = false;
+            grabObject.GetComponent<Rigidbody>().velocity = velocity;
+            
             isTouched = false;
             grabObject = null;
         }
---
+++ Assets/02.Scripts/LaserPointer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour
+{
+    private LineRenderer line;
+    public float maxDistance = 20.0f;
+    public Color laserColor = Color.green;
+
+    private RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateLine();
+    }
+
+    void CreateLine()
+    {
+        line = this.gameObject.AddComponent<LineRenderer>();
+        line.useWorldSpace = false;
+        line.positionCount = 2;
+        line.SetPosition(0, Vector3.zero);
+        line.SetPosition(1, new Vector3(0, 0, maxDistance));
+        line.startWidth = 0.03f;
+        line.endWidth   = 0.005f;
+        line.material = new Material(Shader.Find("Unlit/Color"));
+        line.material.color = laserColor;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance))
+        {
+            line.SetPosition(1, new Vector3(0, 0, hit.distance));
+        }
+    }
+}
